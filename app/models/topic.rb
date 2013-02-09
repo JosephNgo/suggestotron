16f38a945cb065912dd408a6aa4301aebaea75bb
Joseph Ngo <joengo83@gmail.com>
2013-02-09 15:41:41 -0800

    Add vote functionality

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,12 @@
 class Topic < ActiveRecord::Base
   attr_accessible :description, :title
   validates_presence_of :title, :description
-  #apidock.com Hal's favorite resource
-  #www.ruby-doc.org
-  #www.api.rubyonrails.org
+
+  has_many :votes
+ 
 end
  
+
+ #apidock.com Hal's favorite resource
+ #www.ruby-doc.org
+ #www.api.rubyonrails.org
