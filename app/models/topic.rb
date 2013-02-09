742be0217fd3150ee9e6bc517dabc719c6d9dccf
Joseph Ngo <joengo83@gmail.com>
2013-02-09 15:03:10 -0800

    adding validations to topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   attr_accessible :description, :title
+  validates_presence_of :title, :description
+  #apidock.com Hal's favorite resource
+  #www.ruby-doc.org
+  #www.api.rubyonrails.org
 end
+ 
